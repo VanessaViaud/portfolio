7f9f3c0da6599ea45bb19d5734a62caf3abc4f90
VanessaViaud <vanessa.viaud@gmail.com>
2024-09-02 15:32:42 +0200

    fix/adjust_width&responsive_design

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,6 +9,8 @@ function Title() {
   return (
     <div className="presentation">
       <div className="presentation__title">
+        <h1>VANESSA VIAUD</h1>
+        <h2>développeuse web</h2>
         <img
           className="presentation__title__logo"
           src={logo}
